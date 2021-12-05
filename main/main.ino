88226b31585101bf1746265af70f9aaaa3fe2b2f
KevinGuerrero <kev162020@gmail.com>
2021-12-05 03:13:34 -0600

    Version1

---
+++ main/main.ino
@@ -0,0 +1,12 @@
+#include <Motor.h>
+Motor Motor1(2,3,200);
+
+void setup() {
+// put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Motor1.desplegar(250);
+  
+}
