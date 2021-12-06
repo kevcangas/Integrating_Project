277a5ae1641c921c8a795c83b5b5ec10006cc173
KevinGuerrero <kev162020@gmail.com>
2021-12-06 01:50:10 -0600

    Versión 2 con LCD

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,38 @@
 #include <Motor.h>
-Motor Motor1(2,3,200);
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//Instancias a utilizar
+Motor Motores(2,3,200);
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
+//Variable PROVISIONAL para el código
+bool controlM = 0;
 
 void setup() {
-// put your setup code here, to run once:
+  //Inicialización y conf de la pantalla
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+
+  pinMode(13,INPUT); //Señal para activar los motores (PROVISIONAL)
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Motor1.desplegar(250);
+
+  if(controlM == 0 and digitalRead(13) == 1){
+    lcd.setCursor(0,0);
+    lcd.print("Desplegando...");
+    controlM = 1;
+    Motores.desplegar(250);
+  }
+  
+  else if(controlM == 1 and digitalRead(13) == 1){
+    lcd.setCursor(0,0);
+    lcd.print("Contrayendo...");
+    controlM = 0;
+    Motores.contraer(250);
+  }
   
 }
